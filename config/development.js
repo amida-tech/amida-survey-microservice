a8663bb288bfb9a386ee7d64aa54e1e76a997d98
James Fadeley <jmfadeley@gmail.com>
2018-08-02 17:03:36 -0400

    Revert "Feature/devops 270 jf highlander"

--- config/development.js
+++ config/development.js
@@ -2,6 +2,6 @@
 
 module.exports = {
     jwt: {
-        secret: process.env.JWT_SECRET || '0a6b944d-d2fb-46fc-a85e-0295c986cd9f',
+        secret: process.env.SURVEY_SERVICE_CLIENT_SECRET || '0a6b944d-d2fb-46fc-a85e-0295c986cd9f',
     },
 };
